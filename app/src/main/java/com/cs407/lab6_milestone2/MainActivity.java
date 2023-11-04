6270cf92872cb1cea17a17635eca740582c2b1d4
viktorsakman <viktorsakman@gmail.com>
2023-11-04 14:36:22 -0500

    Initial commit

---
+++ app/src/main/java/com/cs407/lab6_milestone2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cs407.lab6_milestone2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
